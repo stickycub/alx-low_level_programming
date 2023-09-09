a7a7ec60c8d14be233a111cc113126276e6b8d80
Benjamin <krazy2cub@gmail.com>
2023-09-09 13:03:50 -0700

    Print Alphabet 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - This is the entry point
+ *
+ * Return: 0 if succesful
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
