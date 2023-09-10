da47c4cf95bf2c60271df10952a3c8c338395f2a
Benjamin <krazy2cub@gmail.com>
2023-09-10 12:11:21 -0700

    Print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - This is the entry point
+ *
+ * Return: 0 if succesful
+ */
+
+int main(void)
+{
+	int c;
+
+	c = 'z';
+		while
+			(c <= 'z') {
+				putchar(c);
+				c--;
+			}
+	putchar('\n');
+	return (0);
+}
