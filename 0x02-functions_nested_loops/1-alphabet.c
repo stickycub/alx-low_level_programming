a83faa6e79ca68bb4c06ee28db8c86475e7135bb
Benjamin <krazy2cub@gmail.com>
2023-09-13 02:48:44 -0700

    Print Lowcase Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - Print all lower case alphabets with a new line
+ *
+ * Return: 0 if successful
+ */
+
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+}
