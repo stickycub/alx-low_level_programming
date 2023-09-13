8fa21cc6a09cd5da5260ca642da9646c6279b6e5
Benjamin <krazy2cub@gmail.com>
2023-09-13 08:39:53 -0700

    24 hours updated

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,8 +16,7 @@ void jack_bauer(void)
 			{
 				for (c = 0; c <= 5; c++)
 				{
-					for (d = 0; d <= 5; d++)
-					{
+					for (d = 0; d <= 9 9 9 9 9 9 9 9 9				{
 						_putchar(a + 'o');
 						_putchar(b + 'o');
 						_putchar(58);
