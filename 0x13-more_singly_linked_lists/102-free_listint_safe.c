74f07ccdc9f5365ccf530dc8972dc7f699caf607
Benjamin <krazy2cub@gmail.com>
2023-10-24 12:20:58 -0700

    free list int update

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,11 +3,10 @@
 
 /**
  * free_listint_safe - Frees a listint_t list safely.
- * @h: Pointer to a pointer to the head of the list.
+ * @head: Pointer to the head of the list.
  *
  * Return: The size of the list that was freed.
  */
-
 size_t free_listint_safe(listint_t **h)
 {
 	listint_t *current, *temp;
