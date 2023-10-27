cbdc455f563dbba5f775dae6891e970ff4c55531
Benjamin <krazy2cub@gmail.com>
2023-10-27 09:17:04 -0700

    set bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,7 +5,8 @@
  * @n: The unsigned long int number to extract the bit from.
  * @index: The index of the bit to get, starting from 0.
  *
- * Return: The value of the bit at the specified index, or -1 if an error occurred.
+ * Return: The value of the bit at the specified index,
+ * or -1 if an error occurred.
  */
 
 int get_bit(unsigned long int n, unsigned int index)
