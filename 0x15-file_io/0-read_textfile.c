60f9112cfc6d21614fe47371a924ac9c0a04ffc4
Benjamin <krazy2cub@gmail.com>
2023-10-31 11:01:53 -0700

    Read Text File updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
 
 /**
  * read_textfile - Reads and prints a text file to standard output.
@@ -12,66 +14,42 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	char *buffer;
+	ssize_t bytes_read, bytes_written;
+
 	if (filename == NULL)
 		return (0);
 
-	int fd = open_file(filename);
-
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
-	char *buffer = malloc(letters);
-
+	buffer = malloc(letters);
 	if (buffer == NULL)
 	{
-		close_file(fd);
+		close(fd);
 		return (0);
 	}
 
-	ssize_t bytes_read = read(fd, buffer, letters);
-
+	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
 	{
-		close_file(fd);
+		close(fd);
 		free(buffer);
 		return (0);
 	}
 
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
-		close_file(fd);
+		close(fd);
 		free(buffer);
 		return (0);
 	}
 
-	close_file(fd);
+	close(fd);
 	free(buffer);
 
 	return (bytes_written);
-}
-
-/**
- * open_file - Opens a file in read-only mode.
- * @filename: The name of the file to open.
- *
- * Return: The file descriptor or -1 on failure.
- */
-
-int open_file(const char *filename)
-{
-	int fd = open(filename, O_RDONLY);
-
-	return (fd);
-}
-
-/**
- * close_file - Closes a file given its file descriptor.
- * @fd: The file descriptor to close.
- */
-
-void close_file(int fd)
-{
-	close(fd);
 }
